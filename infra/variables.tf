60c94cfa1fa890c4c8765eb4280a943ab991c066
Henry-0810 <ml2002stduy@gmail.com>
2025-04-06 20:48:58 +0100

    some changes

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,6 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_session_token" {}
 variable "key_name" {}
+variable "dockerhub_user" {}
+variable "dockerhub_token" {}
+variable "jenkins_data" {}
